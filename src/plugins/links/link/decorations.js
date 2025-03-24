7c81162e73625a1b8833ab214260087920b4a6e2
feraxhp <feraxhp+gh@gmail.com>
2025-03-24 14:41:43 -0500

    ++ link

--- src/plugins/links/link/decorations.js
+++ src/plugins/links/link/decorations.js
@@ -3,80 +3,48 @@ import { hasSelection, visibleNodes } from "../../common/usefull";
 
 export function decorator(view, _) {
     
-    const noIterable = [ "FencedCode" ]
-
-    const urlTypes = {
-        URL: (from, to, view) => Decoration.mark({ 
-            class: "lk-url url", 
-            tagName: "a", 
-            attributes: {
-                href: view.state.doc.sliceString(from, to),
-                target: "_blank",
-            } 
+    const noIterable = [ "FencedCode", "Image", "Link" ]
+    
+    const types = {
+        Link: {
+            marks: ["LinkMark"],
+            url: "URL",
+        }
+    }
+    const decorations = {
+        Link: (from, to, selected) => Decoration.mark({ class: "link " + (selected ? "sw" : "") }).range(from, to),
+        LinkMark: (from, to) => Decoration.mark({ class: "lk-mk" }).range(from, to),
+        TextDec: (from, to, url) => Decoration.mark({ 
+            class: "lk-text url", tagName: "a", 
+            attributes: { href: url, target: "_blank" } 
+        }).range(from, to),
+        URL: (from, to, url) => Decoration.mark({ 
+            class: "lk-url url", tagName: "a", 
+            attributes: { href: url, target: "_blank" } 
         }).range(from, to),
     };
     
-    const stackble = { 
-        Link: true,
-        Blockquote: true,
-    } 
-    
-    const wraper = { 
-        Link: (from, to, selected) => Decoration.mark({ class: "link " + (selected ? "sw" : "") }).range(from, to) 
-    }
-    
-    const marks = {
-        LinkMark: (from, to) => Decoration.mark({ class: "lk-mk" }).range(from, to),
-    }
-    
     const widgets = []; 
-    const stack = [];
-    let url = "";
-    let selected = false;
-    
     visibleNodes(view, {
-        enter: ({ type: { name }, from, to }) => { 
-            if (name in wraper) { selected = hasSelection(view, from, to); }
-            if (name in stackble) stack.push(name);
-            return !(noIterable.includes(name)) 
-        },
-        leave: ({ type: { name }, from, to }) => {
-            if (name in marks) widgets.push(marks[name](from, to))
+        enter: ({ type: { name }, from, to, node }) => { 
             
-            if (name in urlTypes) widgets.push(urlTypes[name](from, to, view));
-            
-            if (
-                stack.slice(-1) in wraper &&
-                name in urlTypes
-            ) url = view.state.doc.sliceString(from, to)
-            
-            if (name in wraper) {
-                const text = view.state.doc.sliceString(from, to);
-                const tag = text.match(/\[(.*)\]/);
-                const tagText = tag ? tag[1] : "";
+            if (name in types) {
+                widgets.push(decorations.Link(from, to, hasSelection(view, from, to)));
                 
-                widgets.push(wraper[name](from, to, selected));
-                if (url !== ""){
-                    const start = text.indexOf(tagText);
-                    widgets.push(textDec(from + start, from + 1 + tagText.length, url));
+                const marks = types[name].marks.flatMap(mark => node.getChildren(mark));
+                widgets.push(...marks.map(({ from, to }) => decorations.LinkMark(from, to)));
+                
+                const url = node.getChild(types[name].url);
+                
+                if ( url !== null ) {
+                    const { from: start, to: end } = url;
+                    widgets.push(decorations.URL(start, end, view.state.sliceDoc(start, end)))
                 }
-                
-                url = ""
-                selected = false;
             }
             
-            if (name in stackble) stack.pop();
-        }
+            return !(noIterable.includes(name)) 
+        },
     });
     
     return Decoration.set(widgets, true);
 }
-
-const textDec = (from, to, url) => Decoration.mark({ 
-    class: "lk-text url", 
-    tagName: "a", 
-    attributes: {
-        href: url,
-        target: "_blank",
-    } 
-}).range(from, to)
