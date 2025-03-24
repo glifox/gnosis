3b1345089653ba0db2f934a9b5cccf774bff47ca
feraxhp <feraxhp+gh@gmail.com>
2025-03-24 14:58:38 -0500

    ++ link

--- src/plugins/links/link/decorations.js
+++ src/plugins/links/link/decorations.js
@@ -26,23 +26,42 @@ export function decorator(view, _) {
     
     const widgets = []; 
     visibleNodes(view, {
-        enter: ({ type: { name }, from, to, node }) => { 
+        enter: ({ type: { name }, from, to, node }) => {            
+            if (!(name in types)) return !(noIterable.includes(name));
             
-            if (name in types) {
-                widgets.push(decorations.Link(from, to, hasSelection(view, from, to)));
-                
-                const marks = types[name].marks.flatMap(mark => node.getChildren(mark));
-                widgets.push(...marks.map(({ from, to }) => decorations.LinkMark(from, to)));
-                
-                const url = node.getChild(types[name].url);
-                
-                if ( url !== null ) {
-                    const { from: start, to: end } = url;
-                    widgets.push(decorations.URL(start, end, view.state.sliceDoc(start, end)))
+            widgets.push(decorations.Link(from, to, hasSelection(view, from, to)));
+            
+            const marks = types[name].marks.flatMap(mark => node.getChildren(mark));
+            widgets.push(...marks.map(({ from, to }) => decorations.LinkMark(from, to)));
+            
+            const url = node.getChild(types[name].url);
+            let tUrl = null;
+            
+            if ( url !== null) {
+                const { from: start, to: end } = url;
+                tUrl = view.state.sliceDoc(start, end);
+                widgets.push(decorations.URL(start, end, tUrl));
+            }
+            
+            if (node.getChild("Image") === null) {
+                const link = view.state.sliceDoc(from, to);
+                const title = link.match(/\[(.*)\]/);
+                if (
+                    title !== null && 
+                    title.length > 1
+                ) {
+                    const text = title[1];
+                    widgets.push(
+                        decorations.TextDec(
+                            from + link.indexOf(text), 
+                            from + 1 + text.length,
+                            tUrl
+                        )
+                    )
                 }
             }
             
-            return !(noIterable.includes(name)) 
+            return false
         },
     });
     
