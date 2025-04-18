3b231cacfcf2e4785e15741804c7143a6d8e11ad
feraxhp <feraxhp+gh@gmail.com>
2025-04-18 18:48:06 -0500

    *+ icons

--- sample/vite.config.js
+++ sample/vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     build: {
-    rollupOptions: {
-        input: {
-        main: 'index.html',
-        try: 'try/index.html'
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                try: 'try/index.html'
+            }
         }
-    }
     }
 });
