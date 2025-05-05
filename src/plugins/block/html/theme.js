32cdd3556ca06399e414a62455982a592a4c50c4
feraxhp <feraxhp+gh@gmail.com>
2025-05-05 14:00:47 -0500

    *+ html render theme

--- src/plugins/block/html/theme.js
+++ src/plugins/block/html/theme.js
@@ -1,3 +1,15 @@
 import { EditorView } from "codemirror"
 
-export const coreTheme = EditorView.baseTheme({})
+export const coreTheme = EditorView.baseTheme({
+    "& .cm-html-widget": {
+        all: "unset",
+        // outline: "red solid 2px",
+    },
+    "& .cm-html-widget > *": {
+        paddingTop: "0",
+        paddingBottom: "0",
+        marginBottom: "0",
+        marginTop: "0",
+    },
+    "& .cm-widgetBuffer": { display: "none" }
+})
