30d0fd4eb62ec47e7acc53e0980914240a810360
feraxhp <feraxhp+gh@gmail.com>
2025-07-16 11:52:46 -0500

    *! list breaking nested codeblocks

--- dev/current.js
+++ dev/current.js
@@ -1,41 +1,38 @@
 export const text = 
 `
-# titulo
-## titulo
-### titulo
-#### titulo
-##### titulo
-###### titulo 
 
-- list 
+- Navigate to the MarkItDown package:
 
-> [!NOTE]
-> > nested note
-> 
+    ~~~sh
+    cd packages/markitdown
+    ~~~
 
-> [!TIP]
-> Critical content demanding immediate user attention due to potential risks.
-> > Normal \`quote\`
-> ~~~javascript
-> console.log("Hello, World!");
-> ~~~
-> > [!NOTE]
-> > nested note
+- Install \`hatch\` in your environment and run tests:
 
-<img height="150px" src="https://avatars.githubusercontent.com/u/202307645?s=200&v=4" alt="example" />
+    ~~~sh
+    pip install hatch  # Other ways of installing hatch: https://hatch.pypa.io/dev/install/
+    hatch shell
+    hatch test
+    ~~~
 
-https://avatars.githubusercontent.com/u/202307645?s=200&v=4
-[sdawda](https://avatars.githubusercontent.com/u/202307645?s=200&v=4)
+    (Alternative) Use the Devcontainer which has all the dependencies installed:
 
-<a href="https://marked.js.org"><img width="60px" height="60px" src="https://marked.js.org/img/logo-black.svg" align="right" /></a>
+    ~~~sh
+    # Reopen the project in Devcontainer and run:
+    hatch test
+    ~~~
 
+- Run pre-commit checks before submitting a PR: \`pre-commit run --all-files\`
 
+### Contributing 3rd-party Plugins
 
-~~~html
-<p align="center">
-    <img height="150px"
-    src="https://avatars.githubusercontent.com/u/202307645?s=200&v=4"
-    alt="example" />
-</p>
-~~~
+You can also contribute by creating and sharing 3rd party plugins. See \`packages/markitdown-sample-plugin\` for more details.
+
+## Trademarks
+
+This project may contain trademarks or logos for projects, products, or services. Authorized use of Microsoft
+trademarks or logos is subject to and must follow
+[Microsoft's Trademark & Brand Guidelines](https://www.microsoft.com/en-us/legal/intellectualproperty/trademarks/usage/general).
+Use of Microsoft trademarks or logos in modified versions of this project must not cause confusion or imply Microsoft sponsorship.
+Any use of third-party trademarks or logos are subject to those third-party's policies.
 `;
