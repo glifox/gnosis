d71e4d4cc08db0b4a1284ebf9796d51e45399df9
feraxhp <feraxhp+gh@gmail.com>
2026-09-23 13:09:16 -0500

    feat: add old local storage key to avoid lossing information

--- web/pages/try/index.ts
+++ web/pages/try/index.ts
@@ -4,6 +4,7 @@ import { Editor } from "../../components/editor";
 const editor = Editor({
   text: '# Write here...',
   save: true,
+  key: "gnosis-try-editor-content",
   extensions: [
     dynamicTheme
   ]
